8f6f7e623b7b22ea19a8ce81540984b5b3f8e5d9
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-07-12 19:52:52 +0200

    FAGet getBinary() - fix variable name

--- faapi/get.py
+++ faapi/get.py
@@ -62,7 +62,7 @@ class FAGet():
 
             for chunk in file_stream.iter_content(chunk_size=1024):
                 file_binary += chunk
-                time.sleep(1/(self.throttle/1024))
+                time.sleep(1/(FAGet.throttle/1024))
 
             self.Log(f'FAGet getBinary -> success')
             return file_binary
