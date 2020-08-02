d4b9bf2885253bcea1642a999b0b9bf8016b6156
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-02 17:27:47 +0200

    parse - use new parse submodule instead of separate get and page

--- faapi/base.py
+++ faapi/base.py
@@ -1,11 +1,15 @@
 from typing import List
+from typing import Optional
 from typing import Union
 
 from .connection import cookies_load
 from .connection import get
+from .connection import get_binary_raw
 from .connection import make_session
-from .get import FAGet
-from .page import FAPage
+from .parse import BeautifulSoup
+from .parse import page_find
+from .parse import page_parse
+from .parse import sub_parse_figure
 from .sub import FASub
 
 
@@ -14,20 +18,19 @@ class FAAPI:
         cookies_l = [] if cookies_l is None else cookies_l
 
         self.session = make_session(cookies_load(cookies_f, cookies_l))
-        self.Get = FAGet(self.session)
-        self.Page = FAPage()
 
-    def get(self, url: str):
-        return get(self.session, url)
+    def get(self, url: str, **params):
+        return get(self.session, url, **params)
 
-    def getParse(self, url: str, **params):
-        return self.Get.getParse(url, **params)
+    def getParse(self, url: str, **params) -> Optional[BeautifulSoup]:
+        res = get(self.session, url, **params)
+        return page_parse(res.text) if res.ok else None
 
     def getSub(self, ID: Union[int, str], file=False):
         assert isinstance(ID, int) or (isinstance(ID, str) and ID.isdigit())
 
-        sub = self.Get.getParse(f"/view/{ID}")
-        sub = FASub(sub, getBinary=self.Get.getBinary)
+        sub = self.getParse(f"/view/{ID}")
+        sub = FASub(sub, getBinary=get_binary_raw)
         if file:
             sub.getFile()
         return sub
@@ -35,14 +38,14 @@ class FAAPI:
     def userpage(self, user: str):
         assert isinstance(user, str)
 
-        page = self.Get.getParse(f"/user/{user}/")
-        usrn = self.Page.pageFind(page, name="title")[0] if page else ""
+        page = self.getParse(f"/user/{user}/")
+        usrn = page_find(page, name="title")[0] if page else ""
         usrn = (
             usrn.text[12:21]
             if not usrn.text.lower().startswith("account disabled")
             else ""
         )
-        desc = self.Page.pageFind(
+        desc = page_find(
             page, name="div", class_="userpage-layout-profile-container link-override"
         )
         desc = desc[0] if desc else ""
@@ -53,49 +56,49 @@ class FAAPI:
         assert isinstance(user, str)
         assert isinstance(page, int) and page >= 1
 
-        subs = self.Get.getParse(f"/gallery/{user}/{page}")
+        subs = self.getParse(f"/gallery/{user}/{page}")
 
-        titl = self.Page.pageFind(subs, name="title")[0].text
+        titl = page_find(subs, name="title")[0].text
         if titl.lower().startswith("account disabled"):
             return [None, 0]
 
-        subs = self.Page.pageFindAll(subs, name="figure") if subs else []
+        subs = page_find(subs, name="figure") if subs else []
 
-        return [self.Page.subParse(subs), page + 1]
+        return [list(map(sub_parse_figure, subs)), page + 1]
 
     def scraps(self, user: str, page: int = 1):
         assert isinstance(user, str)
         assert isinstance(page, int) and page >= 1
 
-        subs = self.Get.getParse(f"/scraps/{user}/{page}")
+        subs = self.getParse(f"/scraps/{user}/{page}")
 
-        titl = self.Page.pageFind(subs, name="title")[0].text
+        titl = page_find(subs, name="title")[0].text
         if titl.lower().startswith("account disabled"):
             return [None, 0]
 
-        subs = self.Page.pageFindAll(subs, name="figure") if subs else []
+        subs = page_find(subs, name="figure") if subs else []
 
-        return [self.Page.subParse(subs), page + 1]
+        return [list(map(sub_parse_figure, subs)), page + 1]
 
     def favorites(self, user: str, page: str = ""):
         assert isinstance(user, str)
         assert isinstance(page, str)
 
-        page = self.Get.getParse(f'/favorites/{user}/{page.strip("/")}')
+        page = self.getParse(f'/favorites/{user}/{page.strip("/")}')
 
-        titl = self.Page.pageFind(page, name="title")[0].text
+        titl = page_find(page, name="title")[0].text
         if titl.lower().startswith("account disabled"):
             return [None, ""]
 
-        subs = self.Page.pageFindAll(page, name="figure") if page else []
+        subs = page_find(page, name="figure") if page else []
         next = (
-            self.Page.pageFind(page, name="a", class_="button mobile-button right")
+            page_find(page, name="a", class_="button mobile-button right")
             if page
             else []
         )
         next = (next[0].get("href", ""))[11 + len(user):] if next else ""
 
-        return [self.Page.subParse(subs), next]
+        return [list(map(sub_parse_figure, subs)), next]
 
     def search(self, **params):
         if "q" not in params:
@@ -103,23 +106,23 @@ class FAAPI:
         elif any(type(v) not in (str, int) for v in params.values()):
             raise TypeError("params values must be of type string or int")
 
-        page = self.Get.getParse("/search/", **params)
+        page = self.getParse("/search/", **params)
 
-        subs = self.Page.pageFindAll(page, name="figure") if page else []
+        subs = page_find(page, name="figure") if page else []
         next = (
-            not bool(self.Page.pageFind(page, name="input", class_="button hidden"))
+            not bool(page_find(page, name="input", class_="button hidden"))
             if subs
             else False
         )
         next = params.get("page", 1) + 1 if next else 0
 
-        return [self.Page.subParse(subs), next]
+        return [list(map(sub_parse_figure, subs)), next]
 
     def checkUser(self, user: str):
         assert isinstance(user, str)
 
-        page = self.Get.getParse(f"/user/{user}/")
-        titl = self.Page.pageFind(page, name="title")
+        page = self.getParse(f"/user/{user}/")
+        titl = page_find(page, name="title")
 
         if not titl:
             return False
@@ -133,8 +136,8 @@ class FAAPI:
     def checkSub(self, ID: Union[int, str]):
         assert isinstance(ID, int) or (isinstance(ID, str) and ID.isdigit())
 
-        page = self.Get.getParse(f"/view/{ID}/")
-        titl = self.Page.pageFind(page, name="title")
+        page = self.getParse(f"/view/{ID}/")
+        titl = page_find(page, name="title")
 
         if not titl:
             return False
