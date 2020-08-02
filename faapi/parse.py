6a4d3469251c4ad8503f4617673bedd1c959479b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-02 19:07:57 +0200

    parse - import only search from re

--- faapi/parse.py
+++ faapi/parse.py
@@ -1,5 +1,4 @@
-import re
-from typing import Any
+from re import search as re_search
 
 from bs4 import BeautifulSoup
 from bs4 import ResultSet
@@ -11,11 +10,11 @@ def page_parse(text: str) -> BeautifulSoup:
 
 
 def sub_parse_figure(figure: Tag) -> dict:
-    sub_id: int = int(re.search(r"sid-(\d+)", figure["id"]).group(1))
+    sub_id: int = int(re_search(r"sid-(\d+)", figure["id"]).group(1))
     sub_caption_links: ResultSet = figure.find("figcaption").findAll("a")
     sub_title: str = sub_caption_links[0].text
     sub_author: str = sub_caption_links[1].text
-    sub_rating: str = re.search(r"r-(\w+)", figure["class"]).group(1)
+    sub_rating: str = re_search(r"r-(\w+)", figure["class"]).group(1)
 
     return {
         "id": sub_id,
