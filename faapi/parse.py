26ff0aee7dd2d291926cd7668d948df111bdc900
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-02 20:07:56 +0200

    parse - use link href to get submission id instead of regex

--- faapi/parse.py
+++ faapi/parse.py
@@ -10,8 +10,8 @@ def page_parse(text: str) -> BeautifulSoup:
 
 
 def sub_parse_figure(figure: Tag) -> dict:
-    sub_id: int = int(re_search(r"sid-(\d+)", figure["id"]).group(1))
     sub_caption_links: ResultSet = figure.find("figcaption").findAll("a")
+    sub_id: int = int(sub_caption_links[0].strip("/").strip("/")[-1])
     sub_title: str = sub_caption_links[0].text
     sub_author: str = sub_caption_links[1].text
     sub_rating: str = re_search(r"r-(\w+)", figure["class"]).group(1)
