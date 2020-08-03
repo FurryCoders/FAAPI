d2642e5a7d558a8b801adb213df86a04f9f9a18c
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-03 04:13:13 +0200

    examples - files are saved inside example directory

--- examples/gallery.py
+++ examples/gallery.py
@@ -1,10 +1,13 @@
+from json import dumps
+from os import path
 from sys import argv
 from typing import List
-from json import dumps
 
 import faapi
 
 if __name__ == '__main__':
+    work_dir: str = path.dirname(path.abspath(__file__))
+
     # Prepare cookies
     #  Populate values using 1st and 2nd argument
     cookies: List[dict] = [
@@ -27,5 +30,5 @@ if __name__ == '__main__':
         gallery.extend(gallery_page)
 
     # Save downloaded submissions into a json file
-    with open(f"{argv[3]}-gallery.json", "w") as f:
+    with open(path.join(work_dir, f"{argv[3]}-gallery.json"), "w") as f:
         f.write(dumps(list(map(dict, gallery)), indent=2))
