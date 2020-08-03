3e161793a2e062ab687a40df0e65d6d89cc47c5b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-08-03 20:25:23 +0200

    parse, sub - move submission figure and page parsers to parse

--- faapi/parse.py
+++ faapi/parse.py
@@ -1,5 +1,76 @@
+from typing import Dict
+from typing import List
+from typing import Union
+
 from bs4 import BeautifulSoup
+from bs4 import ResultSet
+from bs4.element import Tag
+from dateutil.parser import parse as parse_date
 
 
 def page_parse(text: str) -> BeautifulSoup:
     return BeautifulSoup(text, "lxml")
+
+
+def parse_submission_figure(figure_tag: Tag) -> Dict[str, Union[int, str]]:
+    caption_links: ResultSet = figure_tag.find("figcaption").findAll("a")
+
+    id_: int = int(caption_links[0]["href"].strip("/").split("/")[-1])
+    title: str = caption_links[0].text
+    author: str = caption_links[1].text
+    rating: str = figure_tag["class"][0][2:]
+    type_: str = figure_tag["class"][1][2:]
+
+    return {
+        "id": id_,
+        "title": title,
+        "author": author,
+        "rating": rating,
+        "type": type_
+    }
+
+
+def parse_submission_page(sub_page: BeautifulSoup) -> Dict[str, Union[int, str, List[str]]]:
+    if sub_page.find("section", attrs={"class": "notice-message"}):
+        raise Exception("Error: notice-message section found")
+
+    tag_id: Tag = sub_page.find("meta", property="og:url")
+    tag_sub_info: Tag = sub_page.find("div", attrs={"class": "submission-id-sub-container"})
+    tag_title: Tag = tag_sub_info.find("div", attrs={"class": "submission-title"})
+    tag_author: Tag = tag_sub_info.find("a")
+    tag_date: Tag = sub_page.find("span", attrs={"class": "popup_date"})
+    tag_tags: ResultSet = sub_page.find("section", attrs={"class": "tags-row"}).findAll("a")
+    tag_rating: Tag = sub_page.find("div", attrs={"class": "rating"}).find("span")
+    tag_info: ResultSet = sub_page.find("section", attrs={"class": "info text"}).findAll("div")
+    tag_category1: Tag = tag_info[1].find("span", attrs={"class": "category-name"})
+    tag_category2: Tag = tag_info[1].find("span", attrs={"class": "type-name"})
+    tag_species: Tag = tag_info[2].find("span")
+    tag_gender: Tag = tag_info[3].find("span")
+    tag_description: Tag = sub_page.find("div", attrs={"class": "submission-description"})
+    tag_file_url: Tag = sub_page.find("div", attrs={"class": "download"}).find("a")
+
+    id_: int = int(tag_id["content"].strip("/").split("/")[-1])
+    title: str = tag_title.text.strip()
+    author: str = tag_author.text.strip()
+    date: str = parse_date(tag_date["title"].strip()).strftime("%Y-%m-%d")
+    tags: [str] = [t.text.strip() for t in tag_tags]
+    category: str = tag_category1.text.strip() + "/" + tag_category2.text.strip()
+    species: str = tag_species.text.strip()
+    gender: str = tag_gender.text.strip()
+    rating: str = tag_rating.text.strip()
+    description: str = "".join(map(str, tag_description.children)).strip()
+    file_url: str = "https:" + tag_file_url["href"]
+
+    return {
+        "id": id_,
+        "title": title,
+        "author": author,
+        "date": date,
+        "tags": tags,
+        "category": category,
+        "species": species,
+        "gender": gender,
+        "rating": rating,
+        "description": description,
+        "file_url": file_url,
+    }
--- faapi/sub.py
+++ faapi/sub.py
@@ -1,10 +1,9 @@
-from re import search as re_search
 from typing import List
 
-from bs4 import BeautifulSoup
-from bs4 import ResultSet
-from bs4.element import Tag
-from dateutil.parser import parse
+from .parse import BeautifulSoup
+from .parse import Tag
+from .parse import parse_submission_figure
+from .parse import parse_submission_page
 
 
 class SubPartial:
@@ -28,12 +27,13 @@ class SubPartial:
         return repr(dict(self))
 
     def parse_figure_tag(self, figure_tag: Tag):
-        caption_links: ResultSet = figure_tag.find("figcaption").findAll("a")
+        parsed: dict = parse_submission_figure(figure_tag)
 
-        self.id = int(caption_links[0]["href"].strip("/").split("/")[-1])
-        self.title = caption_links[0].text
-        self.author = caption_links[1].text
-        self.rating, self.type = figure_tag["class"][0][2:], figure_tag["class"][1][2:]
+        self.id: int = parsed["id"]
+        self.title: str = parsed["title"]
+        self.author: str = parsed["author"]
+        self.rating: str = parsed["rating"]
+        self.type: str = parsed["type"]
 
 
 class Sub:
@@ -79,29 +79,16 @@ class Sub:
         if self.sub_page.find("section", attrs={"class": "notice-message"}):
             raise Exception("Error: notice-message section found")
 
-        tag_id: Tag = self.sub_page.find("meta", property="og:url")
-        tag_sub_info: Tag = self.sub_page.find("div", attrs={"class": "submission-id-sub-container"})
-        tag_title: Tag = tag_sub_info.find("div", attrs={"class": "submission-title"})
-        tag_author: Tag = tag_sub_info.find("a")
-        tag_date: Tag = self.sub_page.find("span", attrs={"class": "popup_date"})
-        tag_tags: ResultSet = self.sub_page.find("section", attrs={"class": "tags-row"}).findAll("a")
-        tag_rating: Tag = self.sub_page.find("div", attrs={"class": "rating"}).find("span")
-        tag_info: ResultSet = self.sub_page.find("section", attrs={"class": "info text"}).findAll("div")
-        tag_category1: Tag = tag_info[1].find("span", attrs={"class": "category-name"})
-        tag_category2: Tag = tag_info[1].find("span", attrs={"class": "type-name"})
-        tag_species: Tag = tag_info[2].find("span")
-        tag_gender: Tag = tag_info[3].find("span")
-        tag_description: Tag = self.sub_page.find("div", attrs={"class": "submission-description"})
-        tag_file_url: Tag = self.sub_page.find("div", attrs={"class": "download"}).find("a")
+        parsed: dict = parse_submission_page(self.sub_page)
 
-        self.id = int(tag_id["content"].strip("/").split("/")[-1])
-        self.title = tag_title.text.strip()
-        self.author = tag_author.text.strip()
-        self.date = parse(tag_date["title"].strip()).strftime("%Y-%m-%d")
-        self.tags = [t.text.strip() for t in tag_tags]
-        self.category = tag_category1.text.strip() + "/" + tag_category2.text.strip()
-        self.species = tag_species.text.strip()
-        self.gender = tag_gender.text.strip()
-        self.rating = tag_rating.text.strip()
-        self.description = "".join(map(str, tag_description.children)).strip()
-        self.file_url = "https:" + tag_file_url["href"]
+        self.id: int = parsed["id"]
+        self.title: str = parsed["title"]
+        self.author: str = parsed["author"]
+        self.date: str = parsed["date"]
+        self.tags: List[str] = parsed["tags"]
+        self.category: str = parsed["category"]
+        self.species: str = parsed["species"]
+        self.gender: str = parsed["gender"]
+        self.rating: str = parsed["rating"]
+        self.description: str = parsed["description"]
+        self.file_url: str = parsed["file_url"]
