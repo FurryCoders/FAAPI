0bfa63efaf02730bca29fde37bc81448b1775eae
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-02-16 10:40:48 +0100

    __init__ - export all exceptions

--- faapi/__init__.py
+++ faapi/__init__.py
@@ -1,5 +1,6 @@
 from .__version__ import __version__
 from .base import FAAPI
+from .exceptions import *
 from .journal import Journal
 from .submission import Submission
 from .submission import SubmissionPartial
