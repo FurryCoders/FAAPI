8dae31d1c40207d46882122d5708da4dcf2068ce
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-04-21 17:33:29 +0200

    __init__ - do not export exceptions directly

--- faapi/__init__.py
+++ faapi/__init__.py
@@ -1,6 +1,5 @@
 from .__version__ import __version__
 from .base import FAAPI
-from .exceptions import *
 from .journal import Journal
 from .submission import Submission
 from .submission import SubmissionPartial
