3d963210331ab326bd33dd808d28e865bd8107aa
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-04-23 20:55:59 +0200

    submission - use separate base class to implement common methods

--- faapi/submission.py
+++ faapi/submission.py
@@ -12,15 +12,35 @@ from .parse import parse_submission_page
 from .user import UserPartial
 
 
-class SubmissionPartial:
-    def __init__(self, submission_figure: Tag = None):
-        assert submission_figure is None or isinstance(submission_figure, Tag)
-
-        self.submission_figure: Optional[Tag] = submission_figure
-
+class SubmissionBase:
+    def __init__(self):
         self.id: int = 0
         self.title: str = ""
         self.author: UserPartial = UserPartial()
+
+    def __iter__(self):
+        yield "id", self.id
+        yield "title", self.title
+        yield "author", dict(self.author)
+
+    def __repr__(self):
+        return repr(dict(self))
+
+    def __str__(self):
+        return f"{self.id} {self.author} {self.title}"
+
+    @property
+    def url(self):
+        return join_url(root, "view", self.id)
+
+
+class SubmissionPartial(SubmissionBase):
+    def __init__(self, submission_figure: Tag = None):
+        assert submission_figure is None or isinstance(submission_figure, Tag)
+
+        super().__init__()
+
+        self.submission_figure: Optional[Tag] = submission_figure
         self.rating: str = ""
         self.type: str = ""
         self.thumbnail_url: str = ""
@@ -34,16 +54,6 @@ class SubmissionPartial:
         yield "rating", self.rating
         yield "type", self.type
         yield "thumbnail_url", self.thumbnail_url
-
-    def __repr__(self):
-        return repr(dict(self))
-
-    def __str__(self):
-        return f"{self.id} {self.author} {self.title}"
-
-    @property
-    def url(self):
-        return join_url(root, "view", self.id)
 
     def parse(self, submission_figure: Tag = None):
         assert submission_figure is None or isinstance(submission_figure, Tag)
@@ -61,15 +71,13 @@ class SubmissionPartial:
         self.thumbnail_url: str = parsed["thumbnail_url"]
 
 
-class Submission(SubmissionPartial):
+class Submission(SubmissionBase):
     def __init__(self, submission_page: BeautifulSoup = None):
         assert submission_page is None or isinstance(submission_page, BeautifulSoup)
 
-        self.submission_page: Optional[BeautifulSoup] = submission_page
-
         super().__init__()
-        del self.submission_figure
 
+        self.submission_page: Optional[BeautifulSoup] = submission_page
         self.date: datetime = datetime.fromtimestamp(0)
         self.tags: List[str] = []
         self.category: str = ""
