9111a07dda5d46a6636709f41e9df2c7a71016f2
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-12-04 00:10:59 +0100

    __init__ - add __all__ to allow importing exception, connection, and parse

--- faapi/__init__.py
+++ faapi/__init__.py
@@ -5,3 +5,16 @@ from .submission import Submission
 from .submission import SubmissionPartial
 from .user import User
 from .user import UserPartial
+
+__all__ = [
+    "__version__",
+    "FAAPI",
+    "Journal",
+    "Submission",
+    "SubmissionPartial",
+    "User",
+    "UserPartial",
+    "exceptions",
+    "connection",
+    "parse"
+]
