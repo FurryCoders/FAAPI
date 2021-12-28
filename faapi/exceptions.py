82294a33b67172124ed1a915a84bbc4c713283c6
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-12-28 23:06:58 +0100

    exceptions - remove unnecessary pass statements

--- faapi/exceptions.py
+++ faapi/exceptions.py
@@ -2,53 +2,45 @@ class DisallowedPath(Exception):
     """
     The path is not allowed by the robots.txt.
     """
-    pass
 
 
 class ParsingError(Exception):
     """
     An error occurred while parsing the page.
     """
-    pass
 
 
 class Unauthorized(Exception):
     """
     The user is not logged-in.
     """
-    pass
 
 
 class NonePage(ParsingError):
     """
     The parsed page is None.
     """
-    pass
 
 
 class NoTitle(ParsingError):
     """
     The parsed paged is missing a title.
     """
-    pass
 
 
 class DisabledAccount(ParsingError):
     """
     The resource belongs to a disabled account.
     """
-    pass
 
 
 class ServerError(ParsingError):
     """
     The page contains a server error notice.
     """
-    pass
 
 
 class NoticeMessage(ParsingError):
     """
     A notice of unknown type was found in the page.
     """
-    pass
