6e050e0adc4d6c695e085aad4aeffd77d131c1f3
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-04-07 08:28:24 +0200

    __init__ - fix missing JournalPartial export

--- faapi/__init__.py
+++ faapi/__init__.py
@@ -2,6 +2,7 @@ from .__version__ import __version__
 from .base import FAAPI
 from .comment import Comment
 from .journal import Journal
+from .journal import JournalPartial
 from .submission import Submission
 from .submission import SubmissionPartial
 from .user import User
@@ -12,6 +13,7 @@ __all__ = [
     "FAAPI",
     "Comment",
     "Journal",
+    "JournalPartial",
     "Submission",
     "SubmissionPartial",
     "User",
