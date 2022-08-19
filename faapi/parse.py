8f7935c3b68a48813e87094288359ce03ff87631
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-19 19:13:17 +0200

    parse - use Tag.decode_contents instead of .text for proper rendering of BBCode

--- faapi/parse.py
+++ faapi/parse.py
@@ -158,7 +158,7 @@ def html_to_bbcode(html: str, *, convert_special_characters: bool = False) -> st
         for tag in body.select(selector):
             tag.replaceWith(f"[{bbcode}]{html_to_bbcode(inner_html(tag))}[/{bbcode}]")
 
-    html = body.text.strip()
+    html = body.decode_contents()
 
     if convert_special_characters:
         for char, substitution in (
