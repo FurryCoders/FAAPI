4e882887b387e90b9b14ec2bf06c5ef22a34d085
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-09-07 18:45:01 +0200

    parse:html_to_bbcode - use [tag=name.class] for unknown tags

--- faapi/parse.py
+++ faapi/parse.py
@@ -169,9 +169,9 @@ def html_to_bbcode(html: str, *, special_characters: bool = False) -> str:
         if tag.name == "p":
             tag.replaceWith(html_to_bbcode(inner_html(tag)))
         elif not (div_class := tag.attrs.get("class", None)):
-            tag.replaceWith(f"[tag.{tag.name}]{html_to_bbcode(inner_html(tag))}[/tag.{tag.name}]")
+            tag.replaceWith(f"[tag={tag.name}]{html_to_bbcode(inner_html(tag))}[/tag.{tag.name}]")
         else:
-            tag.replaceWith(f"[tag.{tag.name}.{' '.join(div_class) if isinstance(div_class, list) else div_class}]"
+            tag.replaceWith(f"[tag={tag.name}.{' '.join(div_class) if isinstance(div_class, list) else div_class}]"
                             f"{html_to_bbcode(inner_html(tag))}"
                             f"[/tag.{tag.name}]")
 
